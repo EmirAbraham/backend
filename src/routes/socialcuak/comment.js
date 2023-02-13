c1fbba53cc2e88bd5ae8265bcc10c10431786bbb
Santiago Peralta <peraltasantiago@gmail.com>
2023-02-13 18:32:51 -0300

    algo

---
+++ src/routes/socialcuak/comment.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { SocialComment, SocialPost } = require('../../db'); // Importa tus modelos de Sequelize
+
+router.post('/:id/comments', async (req, res) => {
+  try {
+    const post = await SocialPost.findByPk(req.params.id);
+    if (!post) {
+      return res.status(404).send('No se encontró el post');
+    }
+
+    const { content } = req.body;
+    const newComment = await SocialComment.create({content})
+
+    res.send(newComment);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
