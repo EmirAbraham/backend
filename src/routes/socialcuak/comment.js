96fb0f450219f7284b2c9a467a41930f08923cd1
Santiago Peralta <peraltasantiago@gmail.com>
2023-02-13 19:56:12 -0300

    Ruta post de los comments

--- src/routes/socialcuak/comment.js
+++ src/routes/socialcuak/comment.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { SocialComment, SocialPost } = require('../../db'); // Importar modelos de Sequelize
+const { SocialComment, SocialPost } = require('../../db'); // Importar modelos
 
 router.post('/:id/comments', async (req, res) => {
   try {
