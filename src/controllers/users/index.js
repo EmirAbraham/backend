faa0f67deec766fac2baaf5b20bb124437b1d293
Itchy <JimenezGalvezEduardo@gmail.com>
2023-02-14 07:37:54 -0600

    feat(controllers): :sparkles: Eliminación de usuario
    Se agrego controller de eliminación de usuario de la base de datos

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -1,9 +1,11 @@
 const { getUsers } = require('./getUsers.js');
 const { getUserDetails } = require('./getUserDetails.js');
 const { createUser } = require('./createUser.js');
+const { deleteUser } = require("./deleteUser")
 
 module.exports = {
     getUsers,
     getUserDetails,
-    createUser
+    createUser,
+    deleteUser
 }
