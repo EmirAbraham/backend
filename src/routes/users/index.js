0cad15a36c4a03ade0bdc7c6be1201937eed357c
Itchy <JimenezGalvezEduardo@gmail.com>
2023-02-14 09:41:25 -0600

    feat(routes): :sparkles: Nuevo usuario
    Se agregó nueva ruta para modificar el usuario

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,24 +1,30 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const { getUsers, getUserDetails, createUser, deleteUser } = require('../../controllers/users/index.js');
+const {
+  getUsers,
+  getUserDetails,
+  createUser,
+  deleteUser,
+  updateUser,
+} = require("../../controllers/users/index.js");
 
-router.get('/', async (req, res) => {
-    try {
-        const result = await getUsers();
-        res.status(200).json(result);
-    } catch (error) {
-        res.status(400).json({error: error.message});
-    }
+router.get("/", async (req, res) => {
+  try {
+    const result = await getUsers();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
-router.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const result = await getUserDetails(id);
-        res.status(200).json(result);
-    } catch (error) {
-        res.status(400).json({error: error.message});
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await getUserDetails(id);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 router.post("/", async (req, res) => {
@@ -32,12 +38,22 @@ router.post("/", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
-    const {id} = req.params
-    deleteUser(id)
-    res.json("Usuario eliminado")
+    const { id } = req.params;
+    await deleteUser(id);
+    res.json("Usuario eliminado");
   } catch (e) {
-    res.status(400).json({error: error.message});
+    res.status(400).json({ error: error.message });
   }
-})
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await updateUser(id, req.body);
+    res.json("Actualización exitosa");
+  } catch (e) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 module.exports = router;
