3003fe3bcb0ced85269d7ca4b7af393c789b47d9
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-02-14 11:03:11 -0500

    perf(routes): :rocket: Pasarle los parámetross por query al obtener usuarios

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -4,7 +4,7 @@ const { getUsers, getUserDetails, createUser } = require('../../controllers/user
 
 router.get('/', async (req, res) => {
     try {
-        const result = await getUsers();
+        const result = await getUsers(req.query);
         res.status(200).json(result);
     } catch (error) {
         res.status(400).json({error: error.message});
@@ -22,13 +22,13 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  try {
-    const user = await createUser(req.body);
-    res.json(user);
-  } catch (e) {
-    res.status(400).send(e.toString());
-    console.log(e);
-  }
+    try {
+        const user = await createUser(req.body);
+        res.json(user);
+    } catch (e) {
+        res.status(400).send(e.toString());
+        console.log(e);
+    }
 });
 
 module.exports = router;
