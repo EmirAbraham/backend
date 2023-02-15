8e4baa8463958c8e3fa3ff111be4f66197bb088e
Itchy <JimenezGalvezEduardo@gmail.com>
2023-02-15 13:20:56 -0600

    feat(API): :sparkles: Prueba deploy
    Se  realizan modificaciones para el deploy

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const app = require("./src/app");
 const { conn } = require("./src/db");
 
 
-const PORT = 3001;
+const port = process.env.PORT || 3001;
 conn.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
   });
 });
