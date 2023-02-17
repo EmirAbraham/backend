7ee3a16a5b7dcaff19abd71e604054fb90eb3c00
Emir Abraham <rosarino74@gmail.com>
2023-02-17 15:10:45 -0300

    socialcomment en allpost

--- src/controllers/socialcuak/posts/getAllPosts.js
+++ src/controllers/socialcuak/posts/getAllPosts.js
@@ -2,14 +2,26 @@ const {Socialpost, Userdev, Socialcomment} = require('../../../db');
 
 const getAllPosts = async () => {
     const allPosts = await Socialpost.findAll({
-        include: {
-            model: Userdev,
-            attributes: ['name' , 'image'],
-            model: Socialcomment,
-            attributes: ['content', 'likes']
-        }
+        include:[
+            {
+                model: Userdev,
+                attributes: ['name' , 'image']
+            },
+            {
+                model: Socialcomment,
+                attributes: ['content', 'likes']
+            }
+        ]
     })
     return allPosts
 }
-
+// const coment = allPosts.forEach(async element => {
+//     await Socialcomment.findAll({where:{socialpostId:element.id},
+//     include: {
+//         model: Socialcomment,
+//         attributes: ['content', 'likes']
+//     }})
+// });
+// return coment
+// }
 module.exports = {getAllPosts}
