94951566156bed82c08b494bc02e86bfe526247e
Emir Abraham <rosarino74@gmail.com>
2023-02-17 14:30:01 -0300

    fix para que los comentarios aparezcan en allposts

--- src/controllers/socialcuak/posts/getAllPosts.js
+++ src/controllers/socialcuak/posts/getAllPosts.js
@@ -1,10 +1,12 @@
-const {Socialpost, Userdev} = require('../../../db');
+const {Socialpost, Userdev, Socialcomment} = require('../../../db');
 
 const getAllPosts = async () => {
     const allPosts = await Socialpost.findAll({
         include: {
             model: Userdev,
-            attributes: ['name' , 'image']
+            attributes: ['name' , 'image'],
+            model: Socialcomment,
+            attributes: ['content', 'likes']
         }
     })
     return allPosts
