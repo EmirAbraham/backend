74ebee3c97a95d6ff0ffddcb725e0154747839ff
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-02-17 10:43:36 -0500

    feat(API): :sparkles: Creación de usuario con JWT

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,5 +1,11 @@
 const { Router } = require('express');
 const router = Router();
+
+// middlewares
+const { authorization } = require('../../middlewares/auth.js');
+const { check } = require('express-validator');
+
+// controllers
 const {
     getUsers,
     getUserDetails,
@@ -27,14 +33,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
-  try {
-    const user = await createUser(req.body);
-    res.json(user);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post('/', 
+  [
+    check('name', 'name is a required value.').trim().not().isEmpty(),
+    check('email', 'This is not a email format.').isEmail(),
+    check('password', 'password does not allow blank spaces and must be between 8 and 20 character.').matches(/^(?!.*\s)(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/)
+  ],
+  createUser
+);
 
 router.delete('/:id', async (req, res) => {
   try {
