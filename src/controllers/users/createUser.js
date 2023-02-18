57927455b47d2adcbb4f8cf70ab89d39e2ba57ee
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-02-17 19:13:58 -0500

    feat(controllers): :rocket: Creación de las primeras primeras validaciones modularizadas a los controllers createUser y getUserDetails

--- src/controllers/users/createUser.js
+++ src/controllers/users/createUser.js
@@ -6,61 +6,32 @@ const jwt = require('jsonwebtoken');
 
 const createUser = async (req, res) => {
 
-    //revisar si hay errores
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array()});
-
     const { email, password, nickName, name } = req.body;
 
-    try {
-        // Validar si ya hay userdev con email o nickname 
-        const existsEmail = await Userdev.findOne({
-            where: {
-                email: {
-                    [Op.iLike]: `${email.trim()}`}
-                }
-            }
-        );
-        const existsNickName = await Userdev.findOne({
-            where: {
-                nickName: {
-                    [Op.iLike]: `${nickName.trim()}`}
-                }
-            }
-        );
+    // Hashear la password
+    const salt = await bcryptjs.genSalt(10);
+    const newPassword = await bcryptjs.hash(password, salt);
+    
+    // Crear el usuario
+    const newUser = await Userdev.create({ password: newPassword, email, nickName, name });
 
-        if (existsEmail || existsNickName) return res.status(400).json({ msg: "User already exists"});
-        
-        // Hashear la password
-        const salt = await bcryptjs.genSalt(10);
-        const newPassword = await bcryptjs.hash(password, salt);
-        
-        // Crear el usuario
-        const newUser = await Userdev.create({ password: newPassword, email, nickName, name });
-
-        // Crear el payload del JWT
-        const payload = {
-            user: {
-                id: newUser.id
-            }
+    // Crear el payload del JWT
+    const payload = {
+        user: {
+            id: newUser.id
         }
-
-        // Firmar el JWT
-        jwt.sign(payload, process.env.SECRETA, {
-            expiresIn: 172800 // vence en 2 días
-        }, (error, token) => {
-            if (error) throw error;
-
-            //Mensaje de confirmación
-            res.json({token});
-        });
-
-        // Retorno el usuario creado
-        // return res.status(200).json(newUser);
-
-    } catch (error) {
-        res.status(400).json({ error: error.message });
     }
+
+    // Firmar el JWT
+    jwt.sign(payload, process.env.SECRETA, {
+        expiresIn: 172800 // vence en 2 días
+    }, (error, token) => {
+        if (error) throw error;
+
+        //Mensaje de confirmación
+        res.json({token, user: newUser});
+    });
+
 }
   
 module.exports = { createUser };
--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -5,13 +5,19 @@ const router = Router();
 const { authorization } = require('../../middlewares/auth.js');
 const { check } = require('express-validator');
 
+// validators
+const { 
+  validateGetUserDetails, 
+  validateCreateUser 
+} = require('../../validators/users.js');
+
 // controllers
 const {
     getUsers,
     getUserDetails,
     createUser,
     deleteUser,
-    updateUser,
+    updateUser
 } = require('../../controllers/users/index.js');
 
 router.get('/', async (req, res) => {
@@ -23,23 +29,28 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const result = await getUserDetails(id);
-    res.status(200).json(result);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+router.get('/:id', 
+  validateGetUserDetails, 
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await getUserDetails(id);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
 });
 
 router.post('/', 
-  [
-    check('name', 'name is a required value.').trim().not().isEmpty(),
-    check('email', 'This is not a email format.').isEmail(),
-    check('password', 'password does not allow blank spaces and must be between 8 and 20 character.').matches(/^(?!.*\s)(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/)
-  ],
-  createUser
+  validateCreateUser,
+  async (req, res) => {
+    try {
+      const result = createUser(req, res);
+      return result;
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
 );
 
 router.delete('/:id', async (req, res) => {
