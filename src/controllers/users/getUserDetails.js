1371af9e99a8e304949a056282bd42b0316c0670
Emir Abraham <rosarino74@gmail.com>
2023-02-20 11:41:09 -0300

    getUserDetails fix bug

--- src/controllers/users/getUserDetails.js
+++ src/controllers/users/getUserDetails.js
@@ -2,17 +2,17 @@ const { Userdev, Socialpost } = require('../../db.js');
 
 const getUserDetails = async (id) => {
     
-    const result = await Userdev.findByPk( id, {
-        attributes: {
-            exclude: ['active'],
+    const user = await Userdev.findByPk(id);
+
+    const posts = await Socialpost.findAll({
+        where: {
+            userdevId: id,
+            active: true
         },
-        include: {
-            model: Socialpost,
-            where: { active: true },
-            attributes: { exclude: ['active'] }
-        },
-        order: [[{ model: Socialpost }, 'createdAt', 'DESC']],
-    } );
+        order: [['createdAt', 'DESC']]
+    });
+
+    const result = { ...user.dataValues, socialposts: posts.map(el => el.dataValues)}
 
     return result;
 }
