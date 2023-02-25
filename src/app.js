11efb68f2e286ee08b96db239176b3514ae7a654
DarkItchy <100377655+DarkItchy@users.noreply.github.com>
2023-02-24 18:47:55 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes/index.js')
+const cors = require('cors');
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
+app.options('*', cors({
+  allowedHeaders: ['x-auth-token']
+}));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); 
     res.header('Access-Control-Allow-Credentials', 'true');
